ec2b6cbc157be58426db03e640c28177f0a1e141
Garth Johnson <growlf@thenetyeti.com>
2021-08-22 19:19:18 -0700

    quick test of motors

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 255)
+})
+input.onButtonPressed(Button.B, function () {
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 255)
+})
+DFRobotMaqueenPlus.I2CInit()
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
 })
